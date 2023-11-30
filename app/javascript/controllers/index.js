0ce89a72cfd01c66d49f9746fe60f8ab13e8ffca
Nahuel Chaves <nahuel.chaves@gmail.com>
2023-11-29 23:27:48 -0300

    update

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,13 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from './application';
+import { application } from "./application";
 
-import HelloController from './hello_controller';
-application.register('hello', HelloController);
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
 
-import TabsController from './tabs_controller';
-application.register('tabs', TabsController);
+import TabsController from "./tabs_controller";
+application.register("tabs", TabsController);
+
+import DropdownController from "./dropdown_controller";
+application.register("dropdown", DropdownController);
